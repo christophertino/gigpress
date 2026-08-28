2c94f892eed1e2730cda8208dee1bec1a60f89d0
Christopher Tino <christopher_tino@yahoo.com>
2026-08-28 15:22:29 -0400

    set Update URI to prevent updates from the old project

--- gigpress.php
+++ gigpress.php
@@ -10,6 +10,7 @@
  * Domain Path: /langs/
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:  https://github.com/christophertino/gigpress
  */
 
 /*
